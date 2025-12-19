796160920299403bdd45f1d724b3e0d565929508
Joaovitorrr07 <vitorjoaovitor2004@gmail.com>
2025-12-19 10:03:09 -0300

    Ajustes no TaskService e Controller usando DTOs e exceções customizadas

--- src/main/java/br/edu/ufape/todozao/repository/TaskRepository.java
+++ src/main/java/br/edu/ufape/todozao/repository/TaskRepository.java
@@ -2,21 +2,14 @@ package br.edu.ufape.todozao.repository;
 
 import br.edu.ufape.todozao.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long>{
-
-}
-=======
 
 import java.util.List;
-
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
     boolean existsByTitleAndUserId(String title, Long userId);
 
     List<Task> findByUserId(Long userId);
 }
->>>>>>> upstream/main
